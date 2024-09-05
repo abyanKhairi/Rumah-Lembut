6eb1a7c504e0f480c44e6a75adc964a834d9f4d7
abyanKhairi <khairirisha4@gmail.com>
2024-09-05 09:05:33 +0700

    Style : Form responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {  keyframes: {
+      'marquee': {
+        '0%': { transform: 'translateX(100%)' },
+        '100%': { transform: 'translateX(-100%)' },
+      },
+    },
+    animation: {
+      'marquee': 'marquee 10s linear infinite',
+    },
   },
+},
   plugins: [
     require('daisyui'),
   ],
