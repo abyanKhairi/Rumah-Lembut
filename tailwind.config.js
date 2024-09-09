1b57e9df0196ddfb440b364bba570bc853492b92
abyanKhairi <khairirisha4@gmail.com>
2024-09-09 15:36:20 +0700

    Style : Light

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,31 +2,29 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {  keyframes: {
-      'marquee': {
-        '0%': { transform: 'translateX(100%)' },
-        '100%': { transform: 'translateX(-100%)' },
+    extend: {
+      keyframes: {
+        'marquee': {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'marquee': 'marquee 10s linear infinite',
       },
     },
-    animation: {
-      'marquee': 'marquee 10s linear infinite',
-    },
   },
-},
   plugins: [
     require('daisyui'),
   ],
-
   daisyui: {
-  themes: ["light"], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-  darkTheme: false, // name of one of the included themes for dark mode
-  base: true, // applies background color and foreground color for root element by default
-  styled: true, // include daisyUI colors and design decisions for all components
-  utils: true, // adds responsive and modifier utility classes
-  prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-  logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-  themeRoot: ":root", // The element that receives theme color CSS variables
-},
-
+    themes: ["light"],
+    darkTheme: false,
+    base: true,
+    styled: true,
+    utils: true,
+    prefix: "",
+    logs: true,
+    themeRoot: ":root",
+  },
 }
-
